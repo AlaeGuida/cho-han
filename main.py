1b47f2d1ce52acda5bc31f6c345290f5b9dacd13
eryx <alaeddineguida@gmail.com>
2023-08-05 17:02:43 -0400

    update main file

--- main.py
+++ main.py
@@ -1,69 +1,73 @@
 
 import random
 
+intro = ("In this traditional Japanese dice game, "
+         "two dice are rolled in a bamboo cup by the dealer sitting on the floor. "
+         "The player must guess if the dice total to an even (cho) or odd (han) number")
 
-def game_intro():
-    intro = ("In this traditional Japanese dice game, "
-             "two dice are rolled in a bamboo cup by the dealer sitting on the floor. "
-             "The player must guess if the dice total to an even (cho) or odd (han) number")
-
-    return intro
+print(intro)
 
 
 def bet():
 
-    print(game_intro())
+    game_start = ("The dealer swirls the cup and you hear the rattle of dice."
+                  "The dealer slams the cup on the floor, still covering the"
+                  "dice and asks for your bet!")
 
-    user_input = input("You have 5000 Mon. How much do you bet? (or QUIT)\n>")
+    invalid_input = "Please enter a valid amount (1 - 5000)!"
+
+    user_input = input("You have 5000 mon. How much do you bet? (or QUIT)\n> ")
 
     if user_input.isdigit():
-
         try:
             if 0 < int(user_input) <= 5000:
-                play(user_input)
+                print(game_start)
+                return user_input
             else:
-                print("Please enter a valid amount (1 - 5000)")
-                bet()
+                print(invalid_input)
+                play(user_betting)
         except:
             bet()
 
     elif user_input.upper() == 'QUIT':
-        print('Thanks for playing')
-        exit()
-
+        print('Thanks for playing.')
+        quit()
     else:
-        print("Please enter a valid amount (1 - 5000) if you want to play, or 'QUIT' to exit")
+        print(invalid_input)
         bet()
 
 
-def play(user_bet):
-
-    game_start = (" The dealer swirls the cup and you hear the rattle of dice."
-                  "The dealer slams the cup on the floor, still covering the"
-                  "dice and asks for your bet.")
-
-    print(game_start)
+def play(bet_amount):
 
     first_dice = random.randint(1, 6)
     second_dice = random.randint(1, 6)
 
-    result = ""
+    is_even = True
 
-    user_choice = input("   'even'  or  'odd'  ?\n>")
+    if (first_dice + second_dice) % 2 == 0:
+        is_even = True
+    elif (first_dice + second_dice) % 2 == 1:
+        is_even = False
+
+    user_choice = input("   CHO (even)  or  HAN(odd)  ?\n> ")
+
+    user_chose_even = True
+
+    if user_choice.upper() == 'CHO':
+        user_chose_even = True
+    elif user_choice.upper() == 'HAN':
+        user_chose_even = False
+    else:
+        print("Please enter a valid bet!")
+        play(user_betting)
 
     print("The dealer lifts the cup to reveal:\n", "   ", first_dice, " - ", second_dice)
 
-    calc = (first_dice + second_dice) % 2
-
-    if calc == 0:
-        result = "even"
-    elif calc == 1:
-        result = "odd"
-
-    if user_choice.lower() == result:
-        print("You Won", (user_bet * 2), "!")
-    elif user_choice.lower() != result:
+    if user_chose_even == is_even:
+        print("You Won", (int(bet_amount)*2), "mon!")
+    else:
         print("You Lost!")
 
 
-bet()
+user_betting = bet()
+play(user_betting)
