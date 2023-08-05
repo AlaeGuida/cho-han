44600acbb7b9560d06f99ada105d7ba935055f1d
eryx <alaeddineguida@gmail.com>
2023-08-05 16:04:45 -0400

    update main file

--- main.py
+++ main.py
@@ -17,11 +17,12 @@ def bet():
     user_input = input("You have 5000 Mon. How much do you bet? (or QUIT)\n>")
 
     if user_input.isdigit():
+
         try:
             if 0 < int(user_input) <= 5000:
                 play(user_input)
             else:
-                print("Please enter a valid amount 1 - 5000")
+                print("Please enter a valid amount (1 - 5000)")
                 bet()
         except:
             bet()
@@ -31,11 +32,38 @@ def bet():
         exit()
 
     else:
-        print("Please enter a correct amount 1 - 5000 if you want to play, or 'QUIT' to exit")
+        print("Please enter a valid amount (1 - 5000) if you want to play, or 'QUIT' to exit")
         bet()
 
 
+def play(user_bet):
 
-def play(bet):
+    game_start = (" The dealer swirls the cup and you hear the rattle of dice."
+                  "The dealer slams the cup on the floor, still covering the"
+                  "dice and asks for your bet.")
 
-    bet()
+    print(game_start)
+
+    first_dice = random.randint(1, 6)
+    second_dice = random.randint(1, 6)
+
+    result = ""
+
+    user_choice = input("   'even'  or  'odd'  ?\n>")
+
+    print("The dealer lifts the cup to reveal:\n", "   ", first_dice, " - ", second_dice)
+
+    calc = (first_dice + second_dice) % 2
+
+    if calc == 0:
+        result = "even"
+    elif calc == 1:
+        result = "odd"
+
+    if user_choice.lower() == result:
+        print("You Won", (user_bet * 2), "!")
+    elif user_choice.lower() != result:
+        print("You Lost!")
+
+
+bet()
